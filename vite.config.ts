90c1b8e47841dfe0fed6cd6f30d54b12388cbafd
Joshua Hell <joshua@family-hell.de>
2025-05-20 18:47:36 +0200

    Add vite-plugin-lucide-preprocess to enhance dx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [lucidePreprocess(), solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
   },
